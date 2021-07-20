26b2312a5ee31c666220ec9eb264c86ff5741a49
dragon <29299348z@gmail.com>
2021-07-20 20:16:53 +0800

    homework

---
+++ StampTaxHW/StampTaxHW/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace StampTaxHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "確定送出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "用途";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "汽缸CC數／馬達馬力";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 551);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(339, 465);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "取消重填";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "機車",
+            "貨車",
+            "大客車",
+            "自用小客車",
+            "營業用小客車"});
+            this.comboBox1.Location = new System.Drawing.Point(171, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(255, 23);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(171, 65);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(255, 23);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 19);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "全年度";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 49);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 19);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "依期間";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(603, 45);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "貼心小叮嚀：\r\n1.本表試算之稅額僅供參考之用，不做任何證明，實際應納稅額仍應以稽徵機關核定為準。\r\n2. 使用牌照稅稅額對照表及電動車應納稅額表";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(36, 95);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 25);
+            this.dateTimePicker1.TabIndex = 13;
+            this.dateTimePicker1.UseWaitCursor = true;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(191, 95);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(121, 25);
+            this.dateTimePicker2.TabIndex = 14;
+            this.dateTimePicker2.UseWaitCursor = true;
+            this.dateTimePicker2.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "從";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 102);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "至";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(168, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 15);
+            this.label8.TabIndex = 17;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Location = new System.Drawing.Point(94, 114);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 147);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "使用期間";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Location = new System.Drawing.Point(94, 290);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(447, 138);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "用途及排放量";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(94, 521);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(447, 230);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "試算結果";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 763);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ StampTaxHW/StampTaxHW/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StampTaxHW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();        
+             
+            //comboBox1.Items.Add(new MyItem("機車"));
+            //comboBox1.Items.Add(new MyItem("貨車"));
+            //comboBox1.Items.Add(new MyItem("大客車"));
+            //comboBox1.Items.Add(new MyItem("自用小客車"));
+            //comboBox1.Items.Add(new MyItem("營業用小客車"));           
+        }
+
+        //struct MyItem
+        //{
+        //    public MyItem(string displayName)
+        //    {
+        //        DisplayName = displayName;               
+        //    }
+        //    public string DisplayName { get; set; }
+
+        //    public override string ToString()
+        //    {
+        //        return DisplayName;
+        //    }
+        //}
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)   //只是為了跳不同選項
+        {
+            //comboBox2.Text = ((MyItem)comboBox1.SelectedItem).DisplayName;
+            //comboBox2.Items.Add("100CC");
+            //comboBox2.Items.Add("123423354656");
+
+            if (comboBox1.Text == "機車")
+            {
+                comboBox2.Items.Add("150 以下");
+                comboBox2.Items.Add("151cc~ 250cc");
+                comboBox2.Items.Add("251cc~ 500cc");
+                comboBox2.Items.Add("501cc~ 600cc");
+                comboBox2.Items.Add("1201cc~1800cc");
+                comboBox2.Items.Add("1801cc 以上");                
+            }
+           else if (comboBox1.Text == "貨車" || comboBox1.Text == "大客車")
+            {
+                comboBox2.Items.Add("500cc 以下");
+                comboBox2.Items.Add("501cc - 600cc");
+                comboBox2.Items.Add("601cc - 1200cc	");
+                comboBox2.Items.Add("1201cc - 1800cc");
+                comboBox2.Items.Add("1801cc - 2400cc");
+                comboBox2.Items.Add("2401cc - 3000cc");
+                comboBox2.Items.Add("3001cc~3600cc");
+                comboBox2.Items.Add("3601cc~4200cc");
+                comboBox2.Items.Add("4201cc~4800cc");
+                comboBox2.Items.Add("4801cc~5400cc");
+                comboBox2.Items.Add("5401cc~6000cc");
+                comboBox2.Items.Add("6001cc~6600cc");
+                comboBox2.Items.Add("6601cc~7200cc");
+                comboBox2.Items.Add("7201cc~7800cc");
+                comboBox2.Items.Add("7801cc~8400cc");
+                comboBox2.Items.Add("8401cc~9000cc");
+                comboBox2.Items.Add("9001cc~9600cc");
+                comboBox2.Items.Add("9601cc~10200cc");
+                comboBox2.Items.Add("10201cc 以上");              
+            }
+            else if (comboBox1.Text == "自用小客車" || comboBox1.Text == "營業用小客車")
+            {
+                comboBox2.Items.Add("500cc 以下");
+                comboBox2.Items.Add("501cc - 600cc");
+                comboBox2.Items.Add("601cc - 1200cc");
+                comboBox2.Items.Add("1201cc - 1800cc");
+                comboBox2.Items.Add("1801cc - 2400cc");
+                comboBox2.Items.Add("2401cc - 3000cc");
+                comboBox2.Items.Add("3001cc - 4200cc");
+                comboBox2.Items.Add("4201cc - 5400cc");
+                comboBox2.Items.Add("5401cc - 6600cc");
+                comboBox2.Items.Add("6601cc - 7800cc");
+                comboBox2.Items.Add("7801cc 以上");
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Items()
+        {
+            this.comboBox2.Items.Add("150 / 12HP以下(12.5PS以下)");
+            this.comboBox2.Items.Add("151-250 / 12.1-20HP(12.0-20.3PS)");
+            this.comboBox2.Items.Add("251-500 /20.1以上(20.4PS以上)");
+            this.comboBox2.Items.Add("501-600");
+            this.comboBox2.Items.Add("601-1200");
+            this.comboBox2.Items.Add("1801或以上");
+        }
+
+        private void Items2()
+        {
+            this.comboBox2.Items.Add("500cc 以下");
+            this.comboBox2.Items.Add("501cc - 600cc");
+            this.comboBox2.Items.Add("601cc - 1200cc");
+            this.comboBox2.Items.Add("1801cc - 2400cc");
+            this.comboBox2.Items.Add("2401cc - 3000cc");
+            this.comboBox2.Items.Add("3001cc~3600cc");
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Init();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Init();
+            this.comboBox1.Enabled = true;
+            this.comboBox2.Enabled = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //顯示日期
+            this.label6.Text = "從";
+            this.label7.Text = "到 ";
+            this.dateTimePicker1.Visible = true;
+            this.dateTimePicker2.Visible = true;
+            this.comboBox1.Enabled = true;
+            this.comboBox2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Init();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //算式
+            this.label1.Text = 
+                $"使用期間:\r\n" +
+                $"計算天數:  \r\n" +
+                $"汽缸CC數: \r\n用途: \r\n" +
+                $"計算公式: \r\n" +
+                $"應納稅額: 共  元";
+        }
+
+        private double GetTaxPercentage()
+        {
+            string taxType = this.comboBox2.Text;  //判斷下拉選單選到的項目
+
+            switch (taxType)                       //當選到甚麼字就回傳多少
+            {
+                case "150cc 以下": return 0;
+                case "151cc~ 250cc": return 800;
+                case "251cc~ 500cc": return 1620;
+                case "501cc~ 600cc": return 2160;
+                case "601cc~1200cc": return 4320;
+                case "1201cc~1800cc": return 7120;
+                case "1801cc 以上": return 11230;
+                default:return 0;
+            }
+        }
+
+        private void Init()
+        {
+            this.comboBox1.Text = " ";
+            this.comboBox2.Text = " ";
+            this.comboBox1.Enabled = false;
+            this.comboBox2.Enabled = false;
+            this.label6.Text = " ";
+            this.label7.Text = " ";
+            this.dateTimePicker1.Visible = false;
+            this.dateTimePicker2.Visible = false;
+            this.label1.Text = " ";
+        }
+    }
+}
